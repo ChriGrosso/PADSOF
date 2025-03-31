4287619789164cc14bad1234a055676695d0ae90
sofiac1024 <sofiacastro1024@gmail.com>
2025-03-31 17:52:20 +0200

    Panel Login

---
+++ src/interfaz/Aplicacion.java
@@ -0,0 +1,14 @@
+package interfaz;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+
+public class Aplicacion extends JFrame{
+	// Inicializar todos los paneles personalizados
+	
+	public Aplicacion() {}
+}
+
+
